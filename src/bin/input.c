b1d531434ef49c0921b4434d0500d234f5a508de
Nicola Stanislao Vitale <nivit@rocketmail.com>
2015-02-10 15:44:52 +0100

    Fix build on FreeBSD-11
    The H_EVENT constant in <histedit.h> is no longer available.

--- src/bin/input.c
+++ src/bin/input.c
@@ -51,7 +51,7 @@ input(int *cmd)
 				#if __FreeBSD_version >= 500000
 					&he,
 				#endif
-					H_EVENT, 100);
+					H_SETSIZE, 100);
 				el_set(el, EL_HIST, history, hist);
 				el_set(el, EL_EDITOR, "emacs");
 				el_set(el, EL_PROMPT, ufmcontrol_prompt);
